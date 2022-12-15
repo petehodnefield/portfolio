63a78900ede55041b888c9a9b31e8c546e3318a2
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-12-15 11:45:02 -0600

    fixed header link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 import HamIcon from "./components/HamIcon";
 import { HamMenu } from "./components/HamMenu";
 import NavLinks from "./components/NavLinks";
-
+import { Link } from "react-router-dom";
 const Header = ({ isOpen, setIsOpen }) => {
   const [pageSelected, setPageSelected] = useState("");
 
   return (
     <header className="header">
       <h1 className="h1">
-        <a href="/" className="h1-link">
+        <Link to="/" className="h1-link">
           Pete Hodnefield
-        </a>
+        </Link>
       </h1>
       <HamIcon isOpen={isOpen} setIsOpen={setIsOpen}></HamIcon>
       <HamMenu isOpen={isOpen}></HamMenu>
